09a9817b4ca8ea6446afede4789225606ff35e3e
Oliver <contato_limaverde@protonmail.com>
2025-10-09 15:57:14 -0300

    Coded new http methods for the api gateway, refactored existing ones.

--- api/Gateway.java
+++ api/Gateway.java
@@ -1,27 +1,62 @@
 package api;
 
-import system.Contact;
-import system.Number;
 import system.PhoneBook;
+
+import javax.management.InvalidAttributeValueException;
 
 public class Gateway {
 
     private static final PhoneBook generalPhoneBook = new PhoneBook();
 
-    public static void postNewContact(String[] processedContactStrings){
+    public static void POST(String[] newContactData){
 
-        String name = processedContactStrings[0];
-        Number number = new Number(processedContactStrings[1]);
+        String name = newContactData[0];
+        String number = newContactData[1];
 
         generalPhoneBook.addNewContact(name, number);
 
     }
 
-    public static String[] getContact(Number number) {
-        return generalPhoneBook.getContact(number);
+    public static String[] GET(String name, String number) {
+        return generalPhoneBook.getContactData(name, number);
     }
-    public static String[] getContact(String name) {
-        return generalPhoneBook.getContact(name);
+
+    public static void PUT(String[] currentContactData, String newName, String newNumber) throws InvalidAttributeValueException {
+        try {
+            generalPhoneBook.setContact(currentContactData, newName, newNumber);
+        } catch (InvalidAttributeValueException e) {
+            throw e;
+        }
+    }
+
+    public static void PATCH(String[] currentContactData, String newName, String newNumber) throws InvalidAttributeValueException {
+
+        if (newName != null && newNumber != null){ throw new InvalidAttributeValueException(
+                "Somente um atributo pode ser modificado."
+        );}
+
+        try {
+            if (newName != null) {
+                generalPhoneBook.setContact(currentContactData, newName, null);
+            }
+            else if (newNumber != null){
+                generalPhoneBook.setContact(currentContactData, null, newNumber);
+            }
+        } catch (InvalidAttributeValueException e) {
+            throw e;
+        }
+    }
+
+    public static void DELETE(String[] currentContactData) throws InvalidAttributeValueException {
+        String name = currentContactData[0];
+        String number = currentContactData[1];
+
+        try {
+            generalPhoneBook.deleteContact(name, number);
+        }
+        catch (InvalidAttributeValueException e){
+            throw e;
+        }
     }
 
 
